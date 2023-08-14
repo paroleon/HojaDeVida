b2cc92e607ca7899f6ef68ee6531f6b830b430a6
paroleon <75042742+paroleon@users.noreply.github.com>
2023-08-14 08:21:30 -0500

    se cran validaciones.js

--- validacion.js
+++ validacion.js
@@ -1,1 +1,42 @@
 //Haz tú validación en javascript acá
+
+
+/*
+El botón enviar solo debe ser activado cuando todos los campos del formulário estén llenos
+El botón debe enviar el mensaje.*/
+
+const arreglo = [
+    document.getElementById("input-nombre"),
+    document.getElementById("input-email"),
+    document.getElementById("input-asunto"),
+    document.getElementById("mensagem")
+]
+
+const botonEnviar = document.getElementById("boton-enviar")
+
+function validador(arreglo){
+    var allTrue = false;
+    arreglo.forEach(element => {
+        if(element.validity.valid == true){
+            allTrue = true;
+         }else{
+            allTrue = false;
+         }
+    });
+    return allTrue
+}
+
+arreglo.forEach(element => {
+    element.addEventListener("change", (evento) => {
+        
+        if(validador(arreglo)){
+            botonEnviar.disabled = false        
+        } else {
+            botonEnviar.disabled = true
+        }
+    })
+});
+
+botonEnviar.addEventListener("click", () => {    
+    alert("Sus datos han sido enviados")
+})
